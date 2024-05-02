617b25f3f397ce7d00aa787170a505d0caccf5de
csw1511 <csw3411@gmail.com>
2024-05-02 13:10:51 +0900

    [BE] Feat : 회원가입기능 완료

--- BackEnd/src/main/java/com/c206/backend/domain/member/service/MemberServiceImpl.java
+++ BackEnd/src/main/java/com/c206/backend/domain/member/service/MemberServiceImpl.java
@@ -1,5 +1,24 @@
 package com.c206.backend.domain.member.service;
 
+import com.c206.backend.domain.achievement.dto.response.MemberAchievementListResponseDto;
+import com.c206.backend.domain.achievement.entity.Achievement;
+import com.c206.backend.domain.achievement.entity.MemberAchievement;
+import com.c206.backend.domain.achievement.repository.AchievementRepository;
+import com.c206.backend.domain.achievement.repository.MemberAchievementRepository;
+import com.c206.backend.domain.achievement.service.MemberAchievementService;
+import com.c206.backend.domain.member.entity.Member;
+import com.c206.backend.domain.member.entity.MemberInfo;
+import com.c206.backend.domain.member.exception.MemberError;
+import com.c206.backend.domain.member.exception.MemberException;
+import com.c206.backend.domain.member.repository.MemberInfoRepository;
+import com.c206.backend.domain.pet.entity.MemberPet;
+import com.c206.backend.domain.pet.entity.Pet;
+import com.c206.backend.domain.pet.exception.PetError;
+import com.c206.backend.domain.pet.exception.PetException;
+import com.c206.backend.domain.pet.repository.MemberPetRepository;
+import com.c206.backend.domain.pet.repository.PetRepository;
+import com.c206.backend.domain.pet.service.MemberPetService;
+import com.c206.backend.domain.pet.service.MemberPetServiceImpl;
 import com.c206.backend.global.jwt.CustomUserDetailsService;
 import com.c206.backend.domain.member.dto.request.SignInRequestDto;
 import com.c206.backend.domain.member.dto.request.SignUpRequestDto;
@@ -7,15 +26,28 @@ import com.c206.backend.domain.member.repository.MemberRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
+    private final MemberInfoRepository memberInfoRepository;
+    private final AchievementRepository achievementRepository;
+    private final MemberAchievementRepository memberAchievementRepository;
+    private final MemberPetService memberPetService;
+    private final MemberAchievementService memberAchievement;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 
-    public MemberServiceImpl(MemberRepository memberRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public MemberServiceImpl(MemberRepository memberRepository, MemberInfoRepository memberInfoRepository, AchievementRepository achievementRepository, MemberAchievementRepository memberAchievementRepository, MemberPetService memberPetService, MemberAchievementService memberAchievement, MemberAchievementService memberAchievement1, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.memberRepository = memberRepository;
+        this.memberInfoRepository = memberInfoRepository;
+        this.achievementRepository = achievementRepository;
+        this.memberAchievementRepository = memberAchievementRepository;
+        this.memberPetService = memberPetService;
+        this.memberAchievement = memberAchievement1;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
@@ -25,14 +57,56 @@ public class MemberServiceImpl implements MemberService{
         String email = signupDto.getEmail();
         String password = signupDto.getPassword();
 
-        boolean isExist = memberRepository.existsByEmail(email);
+        Optional<Member> isExist = memberRepository.findByEmail(email);
 
-        if(isExist){
+        if(isExist.isPresent()){
+            System.out.println("이미 존재하는 사용자입니다.");
             return false;
         }else{
-
+            System.out.println("회원가입 가능");
+            //회원가입
             signupDto.setPassword(bCryptPasswordEncoder.encode(password));
             memberRepository.save(signupDto.toEntity());
+            System.out.println("회원가입 성공");
+
+            Optional<Member> nowMember = memberRepository.findByEmail(email);
+            if(nowMember.isEmpty()){
+                return false;
+            }
+
+            System.out.println("기본펫 지급시작");
+            //기본펫 1번 지급
+            Member member = memberRepository.findById(nowMember.get().getId()).orElseThrow(()
+                    -> new MemberException(MemberError.NOT_FOUND_MEMBER));
+            memberPetService.provideBasePet(member);
+            System.out.println("기본펫 지급성공");
+
+            System.out.println("회원정보 지정시작");
+            //회원-정보 테이블에 기본사항 지정하기
+            MemberInfo newMemberInfo= MemberInfo.builder()
+                    .member(member)
+                    .profilePetId(null)
+                    .exp(0)
+                    .currency(0)
+                    .build();
+            memberInfoRepository.save(newMemberInfo);
+            System.out.println("회원정보 지정성공");
+
+            System.out.println("회원업적 지정시작");
+            //회원-업적 테이블에 기본사항 지정하기
+            List<Achievement> achievementList = achievementRepository.findAll();
+
+            for (Achievement achievement : achievementList) {
+                MemberAchievement newMemberAchievement = MemberAchievement.builder()
+                        .member(member)
+                        .achievement(achievement)
+                        .progress(0)
+                        .goalMultiply(0)
+                        .build();
+                memberAchievementRepository.save(newMemberAchievement);
+            }
+            System.out.println("회원업적 지정성공");
+
             return true;
         }
     }
@@ -56,4 +130,7 @@ public class MemberServiceImpl implements MemberService{
     public Boolean emailDupCheck(String email) {
         return memberRepository.existsByEmail(email);
     }
+
+
+
 }
