c47773a7aee2b048b9240ef8f146c7516c8cfae4
김민욱 <minwookkim@naver.com>
2024-05-07 14:19:43 +0900

    [FE] Feat : 모달 두개 페이지 라우터(#25)

--- frontend/lib/screens/plogging/progressplogging/component/finishcheck_plogging.dart
+++ frontend/lib/screens/plogging/progressplogging/component/finishcheck_plogging.dart
@@ -1,31 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/core/theme/constant/app_colors.dart';
 import 'package:frontend/core/theme/custom/custom_font_style.dart';
+import 'package:frontend/screens/main/main_screen.dart';
+import 'package:frontend/screens/plogging/finishplogging/finish_plogging_dialog.dart';
 import 'package:go_router/go_router.dart';
 
-class FinishCheckPloggingDialog extends StatelessWidget {
-  const FinishCheckPloggingDialog({super.key});
+class FinishCheckPloggingDialog extends StatefulWidget {
+  final Function onConfirm;
 
+  const FinishCheckPloggingDialog({
+    super.key,
+    required this.onConfirm,
+  });
+
+  @override
+  State<FinishCheckPloggingDialog> createState() =>
+      _FinishCheckPloggingDialogState();
+}
+
+class _FinishCheckPloggingDialogState extends State<FinishCheckPloggingDialog> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       backgroundColor: Colors.white,
-      actions: [
-        ElevatedButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: const Text('취소')),
-        ElevatedButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return const FinishCheckPloggingDialog();
-                },
-              );
-              Navigator.of(context).pop();
-            },
-            child: const Text('확인'))
-      ],
+      content: Row(
+        children: [
+          ElevatedButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('취소')),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                widget.onConfirm();
+              },
+              child: const Text('확인'))
+        ],
+      ),
     );
   }
 }
