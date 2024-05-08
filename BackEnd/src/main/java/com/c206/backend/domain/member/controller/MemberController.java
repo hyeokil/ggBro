f887faf81f352a57fc3a7ad0a29af3abea64e488
csw1511 <csw3411@gmail.com>
2024-05-08 11:28:45 +0900

    [BE] Feat : 전체 잡은 몬스터, 유저 프로필 변경, 펫 닉네임 변경, active true로 바꿔줄 api, pet table 전체조회
    Former-commit-id: ba3ae6bcb8400e9598cd254df71e9df837633816

--- BackEnd/src/main/java/com/c206/backend/domain/member/controller/MemberController.java
+++ BackEnd/src/main/java/com/c206/backend/domain/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.c206.backend.domain.member.controller;
 
 import com.c206.backend.domain.member.dto.request.SignInRequestDto;
 import com.c206.backend.domain.member.dto.request.SignUpRequestDto;
+import com.c206.backend.domain.member.dto.response.MemberTrashCountResDto;
 import com.c206.backend.domain.member.service.MemberService;
 import com.c206.backend.global.common.dto.Message;
 import com.c206.backend.global.jwt.CustomUserDetails;
@@ -79,6 +80,29 @@ public class MemberController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();  // HTTP 500 응답
         }
     }
+
+    @GetMapping("/info")
+    @Operation(summary = "유저 정보를 열람합니다.")
+    public ResponseEntity<Message<?>> getMemberInfo(@Parameter(hidden = true)Authentication authentication){
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+        Long memberId = customUserDetails.getId();
+
+        MemberTrashCountResDto memberTrashCountResDto = memberService.getMemberInfo(memberId);
+
+        return ResponseEntity.ok().body(Message.success(memberTrashCountResDto));
+    }
+
+    @PostMapping("/updateprofile/{profilePetId}")
+    @Operation(summary = "유저 프로필 사진을 업데이트합니다.")
+    public ResponseEntity<Message<?>> updateProfilePic(@Parameter(hidden = true)Authentication authentication, @PathVariable("profilePetId") Long profilePetId){
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+        Long memberId = customUserDetails.getId();
+
+        boolean isUpdated = memberService.updateMemberInfoPicture(memberId, profilePetId);
+
+        return ResponseEntity.ok().body(Message.success(isUpdated));
+    }
+
 
     @PostMapping()
     @Operation(summary = "이메일 중복체크를 진행합니다.")
