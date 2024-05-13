41ec77ad9f7ec19fa782d0f0b47d97678c339aa7
김민욱 <minwookkim@naver.com>
2024-05-13 15:59:47 +0900

    [FE] Feat : 히스토리 진행중(#39)
    Former-commit-id: bd06727bbf7555e52d9526bf1aa63cba4be7582a

--- frontend/lib/models/history_model.dart
+++ frontend/lib/models/history_model.dart
@@ -13,7 +13,7 @@ class HistoryModel with ChangeNotifier {
   HistoryModel(this.userProvider);
   String address = dotenv.get('ADDRESS');
 
-  Map<String, dynamic> member = {};
+  List history = [];
 
   Future<String> getMemberInfo(String accessToken) async {
     var url = Uri.https(address, "/api/v1/member/info");
@@ -25,48 +25,6 @@ class HistoryModel with ChangeNotifier {
     final response = await http.get(url, headers: headers);
 
     if (response.statusCode == 200) {
-      member = json.decode(utf8.decode(response.bodyBytes))["dataBody"];
-      print('응답 $member');
-      return "Success";
-    } else {
-      return "fail";
-    }
-  }
-
-  Map<String, dynamic> getMember() {
-    return member;
-  }
-
-  Future<String> updateProfileImage(String accessToken, int profilePetId) async {
-    print(profilePetId);
-    var url = Uri.https(address, "/api/v1/member/updateprofile/$profilePetId");
-    final headers = {
-      'Content-Type': 'application/json',
-      'Authorization': "Bearer $accessToken"
-    };
-
-    final response = await http.post(url, headers: headers);
-
-    if (response.statusCode == 200) {
-      Fluttertoast.showToast(msg: '변경이 완료되었습니다!');
-      return "Success";
-    } else {
-      print("변경 실패");
-      return "fail";
-    }
-  }
-
-  Future<String> finishTutorial(String accessToken) async {
-    var url = Uri.https(address, "/api/v1/member/tutorial");
-    final headers = {
-      'Content-Type': 'application/json',
-      'Authorization': "Bearer $accessToken"
-    };
-
-    final response = await http.post(url, headers: headers);
-
-    if (response.statusCode == 200) {
-      print("성공");
       return "Success";
     } else {
       return "fail";
