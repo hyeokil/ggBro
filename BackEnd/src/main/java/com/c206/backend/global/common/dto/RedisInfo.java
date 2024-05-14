d1b09628f3c9ccdd3af9334fd109b9b525343d68
csw1511 <csw3411@gmail.com>
2024-05-14 15:02:25 +0900

    [BE] Feat : Redis slave master 시스템 업데이트 및 반영
    Former-commit-id: cedf7696bd80fefaa9e7784a78e18f68c65c476b

---
+++ BackEnd/src/main/java/com/c206/backend/global/common/dto/RedisInfo.java
@@ -0,0 +1,21 @@
+package com.c206.backend.global.common.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ConfigurationProperties(prefix = "spring.data.redis")  // 설정 값을 불러올 때 prefix 값을 지정할 수 있다.
+@Configuration
+public class RedisInfo {
+    private String host;
+    private int port;
+    private RedisInfo master;
+    private List<RedisInfo> slaves;
+}
--- BackEnd/src/main/java/com/c206/backend/global/config/RedisConfig.java
+++ BackEnd/src/main/java/com/c206/backend/global/config/RedisConfig.java
@@ -1,10 +1,15 @@
 package com.c206.backend.global.config;
 
+import com.c206.backend.global.common.dto.RedisInfo;
+import io.lettuce.core.ReadFrom;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.RedisStaticMasterReplicaConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -12,20 +17,63 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableRedisRepositories
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
+//    @Value("${spring.data.redis.master.host}")
+//    private String host_master;
+//
+//    @Value("${spring.data.redis.master.port}")
+//    private int port_master;
+//    @Value("${spring.data.redis.slave-a.host}")
+//    private String host_slave_a;
+//
+//    @Value("${spring.data.redis.slave-a.port}")
+//    private int port_slave_a;
+//    @Value("${spring.data.redis.master.host}")
+//    private String host_slave_b;
+//
+//    @Value("${spring.data.redis.master.port}")
+//    private int port_slave_b;
 
-    @Value("${spring.data.redis.port}")
-    private int port;
+    private final RedisInfo info;
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+////        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+////        redisStandaloneConfiguration.setHostName(host_master);
+////        redisStandaloneConfiguration.setPort(port_master);
+////        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+//
+//        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+//            .readFrom(ReadFrom.REPLICA_PREFERRED)	// replica에서 우선적으로 읽지만 replica에서 읽어오지 못할 경우 Master에서 읽어옴
+//            .build();
+//
+//        RedisStaticMasterReplicaConfiguration slaveConfig = new RedisStaticMasterReplicaConfiguration(host_master, port_master);
+//
+//        slaveConfig.addNode(host_slave_a, port_slave_a);
+//        slaveConfig.addNode(host_slave_b, port_slave_b);
+//
+//        return new LettuceConnectionFactory(slaveConfig, clientConfig);
+//    }
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(host);
-        redisStandaloneConfiguration.setPort(port);
-        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    public LettuceConnectionFactory redisConnectionFactory(){
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+            .readFrom(ReadFrom.REPLICA_PREFERRED)	// replica에서 우선적으로 읽지만 replica에서 읽어오지 못할 경우 Master에서 읽어옴
+            .build();
+
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println(info.getMaster().getHost()+" "+info.getMaster().getPort());
+        System.out.println(info.getSlaves().get(0).getHost()+" "+info.getSlaves().get(0).getPort());
+        System.out.println(info.getSlaves().get(1).getHost()+" "+info.getSlaves().get(1).getPort());
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++");
+        // replica 설정
+        RedisStaticMasterReplicaConfiguration slaveConfig = new RedisStaticMasterReplicaConfiguration(info.getMaster().getHost(), info.getMaster().getPort());
+        // 설정에 slave 설정 값 추가
+        info.getSlaves().forEach(slave -> slaveConfig.addNode(slave.getHost(), slave.getPort()));
+
+
+        return new LettuceConnectionFactory(slaveConfig, clientConfig);
     }
 
     @Bean
