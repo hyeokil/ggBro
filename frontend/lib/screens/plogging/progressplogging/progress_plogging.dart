2035d7654aa08a13f9f9f95f29d15ea004bcd831
김민욱 <minwookkim@naver.com>
2024-05-14 11:04:23 +0900

    [FE] Feat : 플로깅 진행 튜토리얼 진행 중(#39)
    Former-commit-id: 2339cfee1a60228fe4bd4e40f8343e70854794d7

--- frontend/lib/screens/plogging/progressplogging/progress_plogging.dart
+++ frontend/lib/screens/plogging/progressplogging/progress_plogging.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:frontend/provider/user_provider.dart';
 import 'package:frontend/screens/plogging/progressplogging/component/progress_map.dart';
+import 'package:frontend/screens/tutorial/plogging_tutorial_box_dialog.dart';
+import 'package:frontend/screens/tutorial/plogging_tutorial_finish_plogging_dialog.dart';
+import 'package:frontend/screens/tutorial/plogging_tutorial_get_box_dialog.dart';
+import 'package:frontend/screens/tutorial/plogging_tutorial_get_trash_dialog.dart';
+import 'package:frontend/screens/tutorial/plogging_tutorial_kill_trash_dialog.dart';
+import 'package:frontend/screens/tutorial/plogging_tutorial_lacation_dialog.dart';
+import 'package:provider/provider.dart';
 
-class ProgressPlogging extends StatelessWidget {
+class ProgressPlogging extends StatefulWidget {
   // final BluetoothDevice device;
   const ProgressPlogging({
     super.key,
     // required this.device,
   });
+
+  @override
+  State<ProgressPlogging> createState() => _ProgressPloggingState();
+}
+
+class _ProgressPloggingState extends State<ProgressPlogging> {
+  late UserProvider userProvider;
+  late bool memberTutorial;
+
+  @override
+  void initState() {
+    super.initState();
+    userProvider = Provider.of<UserProvider>(context, listen: false);
+    memberTutorial = userProvider.getMemberTutorial();
+
+    if (memberTutorial == false) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return PloggingTutorialLocationDialog();
+          },
+        ).then(
+          (value) {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return PloggingTutorialGetTrashDialog();
+              },
+            );
+          },
+        );
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
