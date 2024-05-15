de27265374a162101ee619dffbca30a14510aadc
kimhyeokil <rlagurdlf@naver.com>
2024-05-15 23:22:25 +0900

    [BE] Feat : pet dummy (#40)
    Former-commit-id: bc63955034d0e4e47e6ce9a40e093779dee28456

--- BackEnd/src/main/resources/sql/data_pet.sql
+++ BackEnd/src/main/resources/sql/data_pet.sql
@@ -4,20 +4,64 @@ INSERT INTO `pet` (
                       `pet_type`
 )
 VALUES (
-    'https://ggbro.s3.ap-northeast-2.amazonaws.com/tnekf.jpg',
+    'https://ggbro.s3.ap-northeast-2.amazonaws.com/pets/00002-1802962273-removebg-preview.png',
         '수달',
         'NORMAL'
 )
      ,(
-    'https://ggbro.s3.ap-northeast-2.amazonaws.com/anfqa.jpg',
-       '물범',
-       'NORMAL'
+    'https://ggbro.s3.ap-northeast-2.amazonaws.com/pets/00003-670815452-removebg-preview.png',
+       '로봇 수달',
+       'ACHIEVEMENT'
 ),(
-    'https://ggbro.s3.ap-northeast-2.amazonaws.com/ghfkddl.jpg',
-    '호랑이',
+    'https://ggbro.s3.ap-northeast-2.amazonaws.com/pets/00005-1479123344-removebg-preview.png',
+    '갈색 로봇 고양이',
     'ACHIEVEMENT'
 ),(
-    'https://ggbro.s3.ap-northeast-2.amazonaws.com/dra.jpg',
-    '용',
+    'https://ggbro.s3.ap-northeast-2.amazonaws.com/pets/6-removebg-preview.png',
+    '먼지 새',
     'ACHIEVEMENT'
+),(
+    'https://ggbro.s3.ap-northeast-2.amazonaws.com/pets/7-removebg-preview.png',
+    '라쿤',
+    'NORMAL'
+),(
+    'https://ggbro.s3.ap-northeast-2.amazonaws.com/pets/00009-2385360464-removebg-preview.png',
+    '도라에몽',
+    'NORMAL'
+),(
+    'https://ggbro.s3.ap-northeast-2.amazonaws.com/pets/00024-141959725-removebg-preview.png',
+    '유치원생',
+    'ACHIEVEMENT'
+),(
+    'https://ggbro.s3.ap-northeast-2.amazonaws.com/pets/00031-895081911-removebg-preview.png',
+    '밤톨이',
+    'ACHIEVEMENT'
+),(
+    'https://ggbro.s3.ap-northeast-2.amazonaws.com/pets/00036-549065694-removebg-preview.png',
+    '어린이',
+    'ACHIEVEMENT'
+),(
+    'https://ggbro.s3.ap-northeast-2.amazonaws.com/pets/intro.png',
+    '맘모스',
+    'NORMAL'
+),(
+    'https://ggbro.s3.ap-northeast-2.amazonaws.com/pets/intro1.png',
+    '꽃사슴',
+    'NORMAL'
+),(
+    'https://ggbro.s3.ap-northeast-2.amazonaws.com/pets/intro2.png',
+    '다람이',
+    'NORMAL'
+),(
+    'https://ggbro.s3.ap-northeast-2.amazonaws.com/pets/intro3.png',
+    '여우',
+    'NORMAL'
+),(
+    'https://ggbro.s3.ap-northeast-2.amazonaws.com/pets/intro4.png',
+    '올뻄',
+    'NORMAL'
+),(
+    'https://ggbro.s3.ap-northeast-2.amazonaws.com/pets/bear.png',
+    '곰',
+    'NORMAL'
 );
