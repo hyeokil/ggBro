a7d655154636f9bb689cf06f8f034931d98404d5
김민욱 <minwookkim@naver.com>
2024-05-18 13:44:51 +0900

    Refactor : 랭킹, 인트로 수정
    Former-commit-id: a317a7ddb76d1afe188a7f0a254fc3c93e003b49

--- frontend/lib/screens/intro/intro_animal_box.dart
+++ frontend/lib/screens/intro/intro_animal_box.dart
@@ -166,7 +166,7 @@ class _IntroScreenState extends State<IntroAnimalBox>
           ),
           Positioned(
             top: MediaQuery.of(context).size.height * 0.2,
-            right: MediaQuery.of(context).size.width * 0.105,
+            right: MediaQuery.of(context).size.width * 0.03,
             child: AnimatedBuilder(
               animation: _scaleAnimation_animal_2!,
               builder: (context, widget) {
@@ -183,14 +183,14 @@ class _IntroScreenState extends State<IntroAnimalBox>
                 color: Colors.transparent,
                 child: Center(
                   child: Image.asset(AppIcons.intro_animal_2,
-                      width: MediaQuery.of(context).size.width * 0.2),
+                      width: MediaQuery.of(context).size.width * 0.3),
                 ),
               ),
             ),
           ),
           Positioned(
             bottom: MediaQuery.of(context).size.height * 0.35,
-            right: MediaQuery.of(context).size.width * 0.27,
+            right: MediaQuery.of(context).size.width * 0.25,
             child: AnimatedBuilder(
               animation: _scaleAnimation_animal_3!,
               builder: (context, widget) {
@@ -207,7 +207,7 @@ class _IntroScreenState extends State<IntroAnimalBox>
                 color: Colors.transparent,
                 child: Center(
                   child: Image.asset(AppIcons.intro_animal_3,
-                      width: MediaQuery.of(context).size.width * 0.2),
+                      width: MediaQuery.of(context).size.width * 0.3),
                 ),
               ),
             ),
@@ -231,14 +231,14 @@ class _IntroScreenState extends State<IntroAnimalBox>
                 color: Colors.transparent,
                 child: Center(
                   child: Image.asset(AppIcons.intro_animal_4,
-                      width: MediaQuery.of(context).size.width * 0.15),
+                      width: MediaQuery.of(context).size.width * 0.2),
                 ),
               ),
             ),
           ),
           Positioned(
             bottom: MediaQuery.of(context).size.height * 0.2,
-            left: MediaQuery.of(context).size.width * 0.2,
+            left: MediaQuery.of(context).size.width * 0.1,
             child: AnimatedBuilder(
               animation: _scaleAnimation_animal_5!,
               builder: (context, widget) {
@@ -255,7 +255,7 @@ class _IntroScreenState extends State<IntroAnimalBox>
                 color: Colors.transparent,
                 child: Center(
                   child: Image.asset(AppIcons.intro_animal_5,
-                      width: MediaQuery.of(context).size.width * 0.25),
+                      width: MediaQuery.of(context).size.width * 0.4),
                 ),
               ),
             ),
