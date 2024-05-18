35b53a6e7f7929f709445e90e9c1a20a418a97fc
kimhyeokil <rlagurdlf@naver.com>
2024-05-19 08:53:33 +0900

    [BE] Fix : fix dummy and time
    Former-commit-id: e8c101734e340b36671e1e0eaea309f8301df91b

--- BackEnd/src/main/resources/sql/data_plogging.sql
+++ BackEnd/src/main/resources/sql/data_plogging.sql
@@ -1,4 +1,4 @@
-INSERT INTO `plogging` (`member_pet_id`, `member_id`, `time`, `distance`)
-VALUES (1, 1, null, 1000),
-(2,2,"13:39 ~ 14:20",430)
+INSERT INTO `plogging` (`member_pet_id`, `member_id`, `time`, `distance`,`created_at`,`updated_at`)
+VALUES (1, 1, null, 1000,NOW(),NOW()),
+(2,2,"13:39 ~ 14:20",430,NOW(),NOW())
 ;
